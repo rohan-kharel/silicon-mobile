2398878a8e0ff31357e50a4ac491a22b189aa4b3
rohan-kharel <111512418+rohan-kharel@users.noreply.github.com>
2025-02-17 10:07:49 +0000

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,51 +1,54 @@
 import { StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const { width: screenWidth } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
-  pageTitle: {
-    fontFamily: "Helvetica",
-    fontSize: 35,
-    fontWeight: "bold",
-    color: "#000050",
-    margin: 20,
-  },
-  articleGallery: {
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  articleGalleryImage: {
-    width: 360,
-    height: 240,
-    borderRadius: 20,
-    alignSelf: "center",
-  },
-  articleGalleryTitle: {
-    marginTop: 10,
-    fontFamily: "Helvetica",
-    fontSize: 30,
-    fontWeight: "bold",
-    color: "#000050",
-    alignSelf: "center",
-  },
-  articleGalleryInfo: {
-    marginTop: 10,
-    fontFamily: "Helvetica",
-    fontSize: 15,
-    fontStyle: "italic",
-    color: "#000050",
-    alignSelf: "center",
-  },
-  articleContainer: {
-    padding: 20,
-  },
-  articleContent: {
-    fontFamily: "Helvetica",
-    fontSize: 18,
-    color: "#000050",
-    marginTop: 20,
-  },
+    screen: {
+        flex: 1,
+    },
+    pageTitle: {
+        fontFamily: "Helvetica",
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "#000050",
+        margin: 20,
+    },
+    articleGallery: {
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    articleGalleryImage: {
+        width: screenWidth > 320 ? screenWidth - 40 : 320,
+        height: screenWidth > 320 ? (screenWidth - 40) * 0.75 : 240,
+        borderRadius: 20,
+        alignSelf: "center",
+    },
+    articleGalleryTitle: {
+        marginTop: 10,
+        fontFamily: "Helvetica",
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "#000050",
+        alignSelf: "center",
+    },
+    articleGalleryInfo: {
+        marginTop: 10,
+        fontFamily: "Helvetica",
+        fontSize: 15,
+        fontStyle: "italic",
+        color: "#000050",
+        alignSelf: "center",
+    },
+    articleContainer: {
+        padding: 20,
+    },
+    articleContent: {
+        fontFamily: "Helvetica",
+        fontSize: 18,
+        color: "#000050",
+        marginTop: 20,
+    },
 });
 
 export default styles;
